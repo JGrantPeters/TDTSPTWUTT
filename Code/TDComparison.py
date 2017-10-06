f2ad9f8a36e3c3f2ba4f877a6d2464025b2d5acf
Jonathan Peters <peters@maths.ox.ac.uk>
2017-10-06 17:26:20 +0100

    Adding the TDComparison

---
+++ Code/TDComparison.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  8 09:53:25 2017
+
+@author: user
+"""
+
+import DTDTSPSTW
+import LTDTSPSTW
+
+
+import numpy as np
+import scipy.optimize as so
+import matplotlib.pyplot as plt
+import cplex
+import scipy.sparse as ss
+import time
+
+#%%
+reload(DTDTSPSTW)
+reload(LTDTSPSTW)
+
+
+#%%
+
+servicetime = 5    
+toy_nnodes = 15;
+
+nltw = 5;
+ndtw = 30
+
+
+hour = 60;
+
+toy_customer_bounds = np.random.uniform(low=0,high=8*hour, size=toy_nnodes-1)
+
+toy_TimeWindows = np.empty([toy_nnodes, 2])
+toy_TimeWindows[0] = np.array([0, 8.5*hour])
+toy_TimeWindows[1:,0] = toy_customer_bounds;
+toy_TimeWindows[1:, 1] = toy_customer_bounds+hour;
+
+linear_toy_travel_times = np.array([[[0 if i==j else np.random.uniform(low = 5, high = hour) for k in range(nltw+1)] for i in range(toy_nnodes) ]for j in range(toy_nnodes)] )
+discrete_toy_travel_times = np.array([[[float(0) for k in range(ndtw+1)] for i in range(toy_nnodes)] for j in range(toy_nnodes)])
+
+discrete_toy_travel_times[:, :, 0] = linear_toy_travel_times[:, :, 0]
+discrete_toy_travel_times[:, :, -1] = linear_toy_travel_times[:, :, -1]
+
+LTW = np.linspace(0, 9.5*hour, nltw+1)
+DTW = np.linspace(0, 9.5*hour, ndtw+1)
+
+for i in range(1,ndtw):
+    interval = None
+    for k in range(nltw):
+        if LTW[k] <= DTW[i] and DTW[i] < LTW[k+1]:
+            interval = k
+    
+    #Now we know the interval i lies in.
+    dt = DTW[i] - LTW[interval]
+    b = linear_toy_travel_times[:, :, interval]
+    slope = np.divide(linear_toy_travel_times[:, :, interval+1] - linear_toy_travel_times[:, :, interval] , 1.0*(LTW[interval+1]- LTW[interval]) )
+    
+    discrete_toy_travel_times[:, :, i] = slope*dt + b;
+    
+    
+
+toy_pen_params = [5, 100, -950]
+
+#%%
+linear = LTDTSPSTW.LTDTSPSTW(toy_nnodes,toy_TimeWindows, linear_toy_travel_times, LTW, servicetime, toy_pen_params)
+discrete = DTDTSPSTW.DTDTSPSTW(toy_nnodes, toy_TimeWindows, discrete_toy_travel_times, DTW, servicetime, toy_pen_params)
+#%%
+a=linear.solve()
+linear.summary()
+#%%
+discrete.solve()
+discrete.summary()
